b6e1ffc49fd0ed828fb11cdc4dedb38cb09ef2d0
Jitender Developer <jitenderdeveloper0741@gmail.com>
2023-02-17 18:26:41 +0530

    api

--- server.js
+++ server.js
@@ -5,3 +5,21 @@ const server = http.createServer(app);
 const PORT = process.env.PORT || 8000;
 
 server.listen(PORT, console.log(PORT,'server started..'))
+
+
+
+// {
+//     "version": 2,
+//     "builds": [
+//         {
+//             "src": "./server.js",
+//             "use": "@vercel/node"
+//         }
+//     ],
+//     "routes": [
+//         {
+//             "src": "/.*",
+//             "dest": "server.js"
+//         }
+//     ]
+// }
